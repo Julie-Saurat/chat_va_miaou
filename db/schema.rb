6be3a99ed63d1806e2c0ada350a733c908a3fb99
Julie Saurat <julie.saurat.s@gmail.com>
2021-02-15 17:43:50 +0100

    cat model set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_155100) do
+ActiveRecord::Schema.define(version: 2021_02_15_163939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cats", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.integer "age"
+    t.string "gender"
+    t.boolean "neutered"
+    t.string "description"
+    t.string "color"
+    t.string "diet"
+    t.string "hair_length"
+    t.string "breed"
+    t.integer "price_per_half_day"
+    t.string "city"
+    t.string "tag"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cats_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,4 +52,5 @@ ActiveRecord::Schema.define(version: 2021_02_15_155100) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cats", "users"
 end
