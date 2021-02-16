bb5c0e148bf172fc017f4597177ffb773f4235f8
Julie Saurat <julie.saurat.s@gmail.com>
2021-02-16 12:29:42 +0100

    Cat Seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "distroying"
+Cat.destroy_all
+
+puts "creating cats"
+5.times do
+  Cat.create!(
+  name: Faker::Creature::Cat.name,
+  age: rand(1..10),
+  gender: ["male", "femelle"].sample,
+  neutered: ["true", "false"].sample == "true",
+  description: Faker::Lorem.paragraph,
+  color: ["brun", "blanc", "noir", "roux", "isabelle"].sample,
+  diet: ["croquette", "pâté", "vegan"].sample,
+  hair_length: ["court", "medium", "long"].sample,
+  breed: Faker::Creature::Cat.breed,
+  price_per_half_day: rand(10..50),
+  city: ["Paris", "Bordeaux", "Lyon", "Toulouse", "Lille", "Marseille", "Brest", "Strasbourg", "Cucuron"].sample,
+  tag: ["calin", "calme", "ronronneur", "espiègle", "gratouilleur", "bavard"].sample,
+  user_id: rand(1..3)
+  )
+end
