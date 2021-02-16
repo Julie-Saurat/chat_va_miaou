c1e8d5a90e1026ee5df27f4cad8afb8bde9a88c6
Charlotte Bory <cmmbory@gmail.com>
2021-02-16 15:26:42 +0100

    Add show action to controller

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,12 @@
 class CatsController < ApplicationController
+before_action :set_cat, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  private
+
+  def set_cat
+    @cat = Cat.find(params[:id])
+  end
 end
