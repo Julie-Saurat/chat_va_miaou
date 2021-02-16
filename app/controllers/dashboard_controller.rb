90948c44bc376a6c84662275aef2943cab168986
Julie Saurat <julie.saurat.s@gmail.com>
2021-02-16 22:52:41 +0100

    page Dashboard accessible

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,10 @@ class DashboardController < ApplicationController
 
   def index
     @cat = "youhou"
+    @cats = policy_scope(Cat)
 
+    
+    # @restaurants = policy_scope(Restaurant).order(created_at: :desc)
   end
 
 end
---
+++ app/policies/dashboard_policy.rb
@@ -0,0 +1,7 @@
+class DashboardPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where(user: user)
+    end
+  end
+end
