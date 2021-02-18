7b3f832ede2225b86290c275e04feba910aa4564
Charlotte Bory <cmmbory@gmail.com>
2021-02-18 14:05:06 +0100

    set dynamic pricing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,6 +27,7 @@ import "bootstrap";
 import { searchCards } from '../components/search';
 import { initMapbox } from '../plugins/init_mapbox';
 import { flatPickr } from "../plugins/flatpickr"
+import { updatePrice } from "../components/booking";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
@@ -36,4 +37,5 @@ document.addEventListener('turbolinks:load', () => {
   searchCards();
   initMapbox();
   flatPickr();
+  updatePrice();
 });
