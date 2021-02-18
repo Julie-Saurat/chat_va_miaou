31b88736a5ee831a415d85e4d432b7bc4a445328
Julie Saurat <julie.saurat.s@gmail.com>
2021-02-18 13:28:28 +0100

    début du css

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,8 +6,6 @@ class DashboardController < ApplicationController
     @bookings = policy_scope(Booking)
     @owner_cat_reservations = @bookings.select { |booking| @owner_cats.include? booking.cat}
     @user_reservations = @bookings.select { |booking| booking.user == current_user}
-    
-    # @restaurants = policy_scope(Restaurant).order(created_at: :desc)
   end
 
 end
