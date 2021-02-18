d36b5c8fd165aa7b486cd348601963a6fbf4eac4
Julie Saurat <julie.saurat.s@gmail.com>
2021-02-18 12:31:36 +0100

    back du dashboard réalisé

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,12 @@ class DashboardController < ApplicationController
 
   def index
     @cats = policy_scope(Cat)
-
+    @owner_cats = current_user.cats
+    @bookings = policy_scope(Booking)
+    @booking = @bookings.first
+    @owner_cat_reservations = @bookings.select { |booking| @owner_cats.include? booking.cat}
+    @user_reservations = @bookings.select { |booking| booking.user == current_user}
+    @renter = @booking.user
     
     # @restaurants = policy_scope(Restaurant).order(created_at: :desc)
   end
